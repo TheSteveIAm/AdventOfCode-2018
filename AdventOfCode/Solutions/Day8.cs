ff1883b66bfef048cbe3a9ecfa7a5df653406cba
TheSteveIAm <TheSteveIAm@Gmail.com>
2018-12-14 11:48:13 -0600

    Day 8 Solutions

--- AdventOfCode/Solutions/Day8.cs
+++ AdventOfCode/Solutions/Day8.cs
@@ -7,14 +7,55 @@ using System.IO;
 
 namespace AdventOfCode
 {
+    class Node
+    {
+        public int nodeId;
+        public int childrenCount;
+        public int metaDataCount;
+        public List<Node> children;
+        public List<int> metaData;
+        public Node parent;
+        //public int nodeValue;
+
+        public Node()
+        {
+            children = new List<Node>();
+            metaData = new List<int>();
+        }
+
+        public int CalcValue()
+        {
+            int value = 0;
+
+            if (childrenCount > 0)
+            {
+                //Console.WriteLine("Node: )F
+                for (int i = 0; i < metaDataCount; i++)
+                {
+                    if (metaData[i] > 0 && metaData[i] <= childrenCount)
+                    {
+                        int reference = metaData[i] - 1;
+                        value += children[reference].CalcValue();
+                    }
+                }
+            }
+            else
+            {
+                value = metaData.Sum();
+            }
+            return value;
+        }
+    }
 
     class Day8
     {
         private string path = "../../Input/input8.txt";
         bool parsed = false;
         string line;
-        int[] inputNums;
-      
+        Queue<int> inputQueue = new Queue<int>();
+        List<Node> nodeTree = new List<Node>();
+        int metaDataSum = 0;
+        int rootValue = 0;
 
         private void ParseInput(string path)
         {
@@ -26,11 +67,10 @@ namespace AdventOfCode
             while ((line = reader.ReadLine()) != null)
             {
                 string[] stringSplit = line.Split(' ').ToArray();
-                inputNums = new int[stringSplit.Length];
 
-                for(int i = 0; i < stringSplit.Length; i++)
+                for (int i = 0; i < stringSplit.Length; i++)
                 {
-                    inputNums[i] = int.Parse(stringSplit[i]);
+                    inputQueue.Enqueue(int.Parse(stringSplit[i]));
                 }
             }
 
@@ -43,14 +83,60 @@ namespace AdventOfCode
         {
             Console.WriteLine("Answer A: ");
             ParseInput(path);
-           
+
+            Node currentNode, childNode;
+
+            //create root node
+            currentNode = new Node()
+            {
+                childrenCount = inputQueue.Dequeue(),
+                metaDataCount = inputQueue.Dequeue(),
+                nodeId = nodeTree.Count
+            };
+
+            nodeTree.Add(currentNode);
+
+            while (inputQueue.Count > 1)
+            {
+                while (currentNode.children.Count < currentNode.childrenCount)
+                {
+                    childNode = new Node()
+                    {
+                        childrenCount = inputQueue.Dequeue(),
+                        metaDataCount = inputQueue.Dequeue(),
+                        parent = currentNode,
+                        nodeId = nodeTree.Count
+                    };
+
+                    nodeTree.Add(childNode);
+
+                    currentNode.children.Add(childNode);
+
+                    currentNode = childNode;
+                }
+
+                if (currentNode.metaDataCount > currentNode.metaData.Count)
+                {
+                    for (int i = 0; i < currentNode.metaDataCount; i++)
+                    {
+                        int metaData = inputQueue.Dequeue();
+                        metaDataSum += metaData;
+                        currentNode.metaData.Add(metaData);
+                    }
+                }
+
+                currentNode = currentNode.parent;
+            }
+            Console.WriteLine(metaDataSum);
         }
 
         public void GetAnswerB()
         {
             Console.WriteLine("Answer B: ");
-            ParseInput(path);
 
+            rootValue = nodeTree[0].CalcValue();
+
+            Console.WriteLine(rootValue);
         }
 
 
